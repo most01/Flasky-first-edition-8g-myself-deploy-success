cf20a65dea17ff06c80431040b5c6fe4ab4783ef
承楹 <weicy@zkteco.com>
2018-04-08 16:08:52 +0800

    config

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-Flasky
-======
+说明：
+1. git clone https://github.com/miguelgrinberg/flasky-first-edition.git
+2. git checkout 8g
+  将这个8g的版本内容复制到桌面，放入flask-8g包
+3. cd flask-8g
+4. heroku create flasky-8g
+5. heroku git:remote -a flasky-8g
+6. heroku addons:create heroku-postgresql:hobby-dev
+7. heroku pg:promote postgresql-objective-28623
+8. git init
+9. git add .
+10.git commit -m 'v1.0'
+11.git push heroku master
+12.heroku run python manage.py deploy
+13.heroku restart
 
-This repository contains the archived source code examples for my O'Reilly book [Flask Web Development](http://www.flaskbook.com), first edition. For the code examples for the current edition of the book, go to [https://github.com/miguelgrinberg/flasky](https://github.com/miguelgrinberg/flasky).
+狗书第一版git checkout 8g, 部署成功的源码
+对接的账号是wcy24@foxmail.com, app是flasky-8g
 
-The commits and tags in this repository were carefully created to match the sequence in which concepts are presented in the book. Please read the section titled "How to Work with the Example Code" in the book's preface for instructions.
 
--- config.py
+++ config.py
@@ -8,15 +8,24 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
+	
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    # FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+	
+	
     MAIL_SERVER = 'smtp.qq.com'
     MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USERNAME = '1300603800@qq.com'
-    MAIL_PASSWORD = 'wsyzywcy'
+    # MAIL_USERNAME = '1300603800@qq.com'
+    # MAIL_PASSWORD = 'wsyzywcy'
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    # FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_MAIL_SENDER = 'Flasky Admin <1300603800@qq.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_ADMIN = '1300603800@qq.com'
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
